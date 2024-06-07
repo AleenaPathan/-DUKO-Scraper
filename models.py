3b364b7542200ff7ff3c9cb166a0eb66901b7a3f
Aleenaaaa.___ <157631324+AleenaPathan@users.noreply.github.com>
2024-06-07 12:29:26 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,13 +1,10 @@
-class Cryptocurrency:
-    def __init__(self, name, symbol, market_cap, volume_24h, circulating_supply, total_supply, max_supply, fully_diluted_market_cap):
-        self.name = name
-        self.symbol = symbol
-        self.market_cap = market_cap
-        self.volume_24h = volume_24h
-        self.circulating_supply = circulating_supply
-        self.total_supply = total_supply
-        self.max_supply = max_supply
-        self.fully_diluted_market_cap = fully_diluted_market_cap
-
-    def __str__(self):
-        return f"{self.name} ({self.symbol}): {self.market_cap}"
+class DUKO:
+    def __init__(self):
+        self.name = None
+        self.price = None
+        self.market_cap = None
+        self.volume_24h = None
+        self.circulating_supply = None
+        self.total_supply = None
+        self.max_supply = None
+        self.fully_diluted_market_cap = None
