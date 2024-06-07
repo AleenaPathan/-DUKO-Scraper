c2e7cb05f70ef25fd862427db70a7c8876cb9143
Aleenaaaa.___ <157631324+AleenaPathan@users.noreply.github.com>
2024-06-07 12:13:40 +0530

    Update and rename serializers.py to scraper.py:

--- serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from.models import Coin
-
-class CoinSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Coin
-        fields = ['name', 'ymbol', 'price', 'price_change', 'arket_cap', 'arket_cap_rank', 'volume', 'volume_rank', 'volume_change', 'circulating_supply', 'total_supply', 'diluted_market_cap', 'contracts', 'official_links', 'ocials']
-      
